67b87be51b169978cfe0b445d58043f551961a81
QJW <434329188@qq.com>
2019-01-30 13:48:11 +0800

    添加UVRect使用实例

---
+++ Assets/UVRect/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public RawImage m_barUI;
+
+	private void Start () {
+		m_barUI = GetComponent<RawImage> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		var rect = m_barUI.uvRect;
+		rect.x -= 0.02f;
+		m_barUI.uvRect = rect;
+	}
+}
